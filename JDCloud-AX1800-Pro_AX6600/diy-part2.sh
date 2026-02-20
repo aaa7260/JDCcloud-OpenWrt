30f5c5e5dd65c8274ff43cec4844f7344c5a2a7d
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2026-02-20 17:24:39 +0800

    Update diy-part2.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -22,13 +22,13 @@ rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 # 确保系统能正确找到雅典娜的无线校准数据
 # sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
 # 1. 修正 U-boot 环境变量工具定义
-sed -i '/redmi,ax5-jdcloud)/i \jdcloud,ax6600|\\\njdcloud,ax1800-pro|\\' package/boot/uboot-envtools/files/qualcommax
+# sed -i '/redmi,ax5-jdcloud)/i \jdcloud,ax6600|\\\njdcloud,ax1800-pro|\\' package/boot/uboot-envtools/files/qualcommax
 
 # 2. 修正无线校准数据识别路径 (防止 B 站断流)
 # 我们用 find 确保能抓到 11-ath11k-caldata 文件
-find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
-find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
+# find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
+# find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
 
 # 3. 修正网络接口映射
-find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
-find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
+# find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
+# find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
