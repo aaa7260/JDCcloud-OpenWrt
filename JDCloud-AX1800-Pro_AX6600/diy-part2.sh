56f75dde219140673fdb3abe534b5bca5630babb
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2026-02-20 17:10:05 +0800

    Update diy-part2.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -21,13 +21,14 @@ mv /tmp/clash package/kenzo/luci-app-openclash/root/etc/openclash/core/clash_met
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 # 确保系统能正确找到雅典娜的无线校准数据
 # sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
-# 修正雅典娜/亚瑟的无线校准数据识别，防止 B 站断流
-sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
-sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
-
-# 修正 U-boot 环境工具定义
+# 1. 修正 U-boot 环境变量工具定义
 sed -i '/redmi,ax5-jdcloud)/i \jdcloud,ax6600|\\\njdcloud,ax1800-pro|\\' package/boot/uboot-envtools/files/qualcommax
 
-# （可选）让固件显示的型号名更好看
-# sed -i 's/DEVICE_MODEL := AX6600/DEVICE_MODEL := Athena-AX6600/g' target/linux/qualcommax/image/ipq60xx.mk
-# sed -i 's/DEVICE_MODEL := AX1800 Pro/DEVICE_MODEL := Arthur-AX1800-Pro/g' target/linux/qualcommax/image/ipq60xx.mk
+# 2. 修正无线校准数据识别路径 (防止 B 站断流)
+# 我们用 find 确保能抓到 11-ath11k-caldata 文件
+find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
+find target/linux/qualcommax/ -name "11-ath11k-caldata" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
+
+# 3. 修正网络接口映射
+find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' {}
+find target/linux/qualcommax/ -name "02_network" | xargs -i sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' {}
