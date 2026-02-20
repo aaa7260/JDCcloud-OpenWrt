2ed6ebfe330e67cba1c57bf24c9343eed2e8d739
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2026-02-20 17:01:50 +0800

    Modify calibration data and model names in scripts
    Update wireless calibration data and U-boot environment definitions for AX6600 and AX1800 Pro models.

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -20,4 +20,14 @@ mkdir -p package/kenzo/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash package/kenzo/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 # 确保系统能正确找到雅典娜的无线校准数据
+# sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
+# 修正雅典娜/亚瑟的无线校准数据识别，防止 B 站断流
 sed -i 's/jdcloud,re-cs-02/jdcloud,ax6600/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
+sed -i 's/jdcloud,re-ss-01/jdcloud,ax1800-pro/g' target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
+
+# 修正 U-boot 环境工具定义
+sed -i '/redmi,ax5-jdcloud)/i \jdcloud,ax6600|\\\njdcloud,ax1800-pro|\\' package/boot/uboot-envtools/files/qualcommax
+
+# （可选）让固件显示的型号名更好看
+sed -i 's/DEVICE_MODEL := AX6600/DEVICE_MODEL := Athena-AX6600/g' target/linux/qualcommax/image/ipq60xx.mk
+sed -i 's/DEVICE_MODEL := AX1800 Pro/DEVICE_MODEL := Arthur-AX1800-Pro/g' target/linux/qualcommax/image/ipq60xx.mk
